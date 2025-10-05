2efbb1c7ef68617a368964d071e8b97ad577e816
lagharimuhammad <lagharimbaqar@gmail.com>
2025-10-05 22:28:18 +0500

    Add files via upload

---
+++ LAB#4/T6.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+
+class Employee(ABC):
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = float(salary)
+
+    @abstractmethod
+    def calculateBonus(self):
+        pass
+
+
+class Manager(Employee):
+    def __init__(self, name, salary):
+        super().__init__(name, salary)
+
+    def calculateBonus(self):
+        return self.salary * 0.20
+
+    def hire(self, personName="someone"):
+        print(f"{self.name} is hiring {personName}.")
+
+
+class Developer(Employee):
+    def __init__(self, name, salary):
+        super().__init__(name, salary)
+
+    def calculateBonus(self):
+        return self.salary * 0.10
+
+    def writeCode(self, feature="a feature"):
+        print(f"{self.name} is writing code for {feature}.")
+
+
+class SeniorManager(Manager):
+    def __init__(self, name, salary):
+        super().__init__(name, salary)
+
+    def calculateBonus(self):
+        return self.salary * 0.30
+
+
+if __name__ == "__main__":
+    m = Manager("Omer", 100000)
+    d = Developer("Kashif", 80000)
+    s = SeniorManager("Baqar", 150000)
+
+    print(f"{m.name} bonus: ${m.calculateBonus():.2f}")
+    m.hire("Umair")
+
+    print(f"{d.name} bonus: ${d.calculateBonus():.2f}")
+    d.writeCode("Database System")
+
+    print(f"{s.name} bonus: ${s.calculateBonus():.2f}")
+    s.hire("Burhan")
